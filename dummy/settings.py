b540c208c78671935a9f24734af1259f3708281e
Arpan Shrestha <arpanshrestha12@gmail.com>
2025-08-29 16:10:56 +0545

    Added Docker

--- dummy/settings.py
+++ dummy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pswmf9zn0y(9^a!ay0zw=6)w7#q2oav25g6yomo)+%@obiz4a5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','*']
 
 
 # Application definition
